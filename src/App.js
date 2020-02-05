f4dcf01d09056eed19755c41d9ab202ef2ce0c44
Isaiah <anyiminene@gmail.com>
2020-02-04 20:40:51 -0800

    added deletion functionability

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([
+	  {id: 1, content: 'Meet Masao for Udon'},
+	  {id: 2, content: 'Fix up these horrible react notes'},
+  ]);
+  function deleteTodo(id) {
+	  setTodos(todos => todos.filter(todo => todo.id !== id));
+  }
   return (
-    <div className="App">
-      Things to do
+	<div className="App">
+      <h1>Things to do</h1>
+      <TodoList todos={todos} deleter={deleteTodo}/>
     </div>
   );
 }
-
-export default App;
